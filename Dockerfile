9b05c231a9e3ff31a41a34f7fdad006600bb9ba8
mluk <mluk@users.noreply.github.com>
2016-04-24 10:00:28 +0200

    ssh key added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ubuntu:latest
 RUN apt-get -y install openssh-server
+COPY id_rsa.pub /.
 COPY init.sh /.
 RUN /bin/sh init.sh
 #RUN service ssh start && service rsyslog restart
--- init.sh
+++ init.sh
@@ -2,3 +2,8 @@ if [ ! -d /var/run/sshd ]; then
    mkdir /var/run/sshd
    chmod 0755 /var/run/sshd
 fi
+
+useradd maik
+mkhomedir_helper maik
+mkdir /home/maik/.ssh
+cat /id_rsa.pub > /home/maik/.ssh/authorized_keys
